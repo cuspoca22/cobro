6afd8f701954a34df550d73c94940f9437947e88
david cuspoca <david-cuspoca@hotmail.com>
2025-10-18 09:52:59 -0600

    app.module.ts
    se quitaron comentarios de codigos

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import mongoose from 'mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { join } from 'path';
 
 import { RutaModule } from './ruta/ruta.module';
 import { CajaModule } from './caja/caja.module';
@@ -10,9 +11,7 @@ import { AuthModule } from './auth/auth.module';
 import { EmpresaModule } from './empresa/empresa.module';
 import { ClienteModule } from './cliente/cliente.module';
 import { CreditoModule } from './credito/credito.module';
-import { join } from 'path';
 import { PruebasModule } from './pruebas/pruebas.module';
-import { FilesModule } from './files/files.module';
 import { LogAuthModule } from './log-auth/log-auth.module';
 import { MessageModule } from './message/message.module';
 import { MovimientoCajaModule } from './movimientoCaja/movimiento-caja.module';
@@ -29,15 +28,13 @@ import { MovimientoCajaModule } from './movimientoCaja/movimiento-caja.module';
     MovimientoCajaModule,
     CreditoModule,
     AuthModule,
-    // RutaModule,
-    // CajaModule,
-    // EmpresaModule,
+    RutaModule,
+    CajaModule,
+    EmpresaModule,
     ClienteModule,
     PruebasModule,
-    // FilesModule,
-    // LogAuthModule,
-    // MessageModule,
-    // ReportsModule,
+    LogAuthModule,
+    MessageModule,
   ],
   controllers: [],
   providers: [],
