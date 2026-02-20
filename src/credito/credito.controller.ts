2477dceae08a154cd72d83ecd85e524041b7aa8e
david cuspoca <david.cuspoca.98@gmail.com>
2026-02-19 19:59:07 -0600

    refactor(credito): limpieza de codigo comentado en controlador

--- src/credito/credito.controller.ts
+++ src/credito/credito.controller.ts
@@ -1,26 +1,22 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Query } from '@nestjs/common';
 
 import { CreditoService } from './credito.service';
-import { CreateCreditoDto, UpdateCreditoDto } from './dto/';
 import { Auth, GetUser } from '../auth/decorators';
 import { ParseMongoIdPipe } from 'src/common/pipes/parse-mongo-id.pipe';
 import { GetUserDto } from '../auth/dto/get-user.dto';
+import { UpdateCreditoDto } from './dto';
 
 @Auth()
 @Controller('credito')
 export class CreditoController {
-  constructor(private readonly creditoService: CreditoService) {}
 
-  // @Post()
-  // create(@Body() createCreditoDto: CreateCreditoDto) {
-  //   return this.creditoService.create(createCreditoDto);
-  // }
+  constructor(private readonly creditoService: CreditoService) { }
 
   // Este sera el enpoint que se llamara para la parte de rutero en el cliente
   @Get('get-creditos-by-ruta')
   async getCreditosByRuta(
     @GetUser() user: GetUserDto
-  ){
+  ) {
     return await this.creditoService.getCreditosByRuta(user.ruta)
   }
 
@@ -40,44 +36,12 @@ export class CreditoController {
     return this.creditoService.getCreditoById(creditId, user.ruta);
   }
 
-  // @Get()
-  // async findAll(
-  //   @Query() paramsDto: ParamsDto,
-  // ) {
-  //   return this.creditoService.getCreditDetailsWithCalculatedData(paramsDto);
-  // }
+  @Patch('turno/:creditoId')
+  async updateTurno(
+    @Param('creditoId', ParseMongoIdPipe) creditoId: string,
+    @Body() updateCreditoDto: UpdateCreditoDto,
+  ) {
+    return await this.creditoService.updateTurno(creditoId, updateCreditoDto);
+  }
 
-  // @Get('renovaciones')
-  // async findRenovaciones(
-  //   @Query('fecha') fecha: string,
-  //   @GetUser() user: User
-  // ) {
-  //   return this.creditoService.findRenovaciones(fecha, user);
-  // }
-
-  
-
-  // @Patch(':id')
-  // async update(
-  //   @Param('id') id: string, 
-  //   @Body() updateCreditoDto: UpdateCreditoDto,
-  //   @Query('fecha') fecha: string,
-  // ) {
-  //   return await this.creditoService.update(id, updateCreditoDto, fecha);
-  // }
-
-  // @Patch('turno/:id')
-  // async updateTurno(
-  //   @Param('id') id: string, 
-  //   @Body() updateCreditoDto: UpdateCreditoDto,
-  // ) {
-  //   return await this.creditoService.updateTurno( id, updateCreditoDto );
-  // }
-
-  // @Delete(':id')
-  // remove(
-  //   @Param('id', ParseMongoIdPipe) id: string,
-  // ) {
-  //   return this.creditoService.remove(id);
-  // }
 }
